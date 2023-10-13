62a03d3c976789ecb2b507b6c264f7a139d6d08d
saleem siddique <saleemprogramacion04@gmail.com>
2023-10-13 13:55:09 +0200

    Empezando Cliente

--- AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/domain/Cliente.java
+++ AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/domain/Cliente.java
@@ -1,4 +1,21 @@
 package com.es.iesmz.transita3.Transita.domain;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "cliente")
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 }
