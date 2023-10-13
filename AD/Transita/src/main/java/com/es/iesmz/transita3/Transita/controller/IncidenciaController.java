69a68b925770719a3aae36cdc850d8e348bd082b
isaac916 <bei8283uu@>
2023-10-13 13:54:09 +0200

    Creacion de las clases

---
+++ AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/controller/IncidenciaController.java
@@ -0,0 +1,4 @@
+package com.es.iesmz.transita3.Transita.controller;
+
+public class IncidenciaController {
+}
---
+++ AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/service/IncidenciaServiceImpl.java
@@ -0,0 +1,4 @@
+package com.es.iesmz.transita3.Transita.service;
+
+public class IncidenciaServiceImpl {
+}
