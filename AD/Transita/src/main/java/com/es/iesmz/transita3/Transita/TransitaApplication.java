28092dc29c023fa30e8542f41cb5cce52f1211ab
saleem siddique <saleemprogramacion04@gmail.com>
2023-10-16 13:33:15 +0200

    Cliente terminado. Version 1.0

--- AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/TransitaApplication.java
+++ AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/TransitaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TransitaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TransitaApplication.class, args);
     }
