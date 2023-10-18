117ee86be3a840ade22256a5ca63e654f619e7b6
saleemsiddique <saleemprogramacion04@gmail.com>
2023-10-18 17:51:57 +0200

    update gitignore

--- AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/TransitaApplication.java
+++ AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/TransitaApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TransitaApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(TransitaApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(TransitaApplication.class, args);
     }
 
 }
