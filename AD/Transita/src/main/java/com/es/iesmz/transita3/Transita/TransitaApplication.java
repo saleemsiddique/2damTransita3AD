df15dff05e3f3b9ddcc19555fc3cb98b327a3556
Isaac916 <tojito916@gmail.com>
2024-01-25 13:44:24 +0100

    No funciona pero dependencias corregidas

--- AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/TransitaApplication.java
+++ AD/Transita/src/main/java/com/es/iesmz/transita3/Transita/TransitaApplication.java
@@ -3,11 +3,17 @@ package com.es.iesmz.transita3.Transita;
 import com.es.iesmz.transita3.Transita.Utils.Util;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TransitaApplication {
+public class TransitaApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(TransitaApplication.class, args);
         Util.readConfigFile();
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(TransitaApplication.class);
     }
 }
